19aa690b28f5356bf4e841b007c67f92fe8c2ee1
Superman <703353105@qq.com>
2018-07-02 13:38:25 +0800

    Initial Commit

---
+++ RecoardTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecoardTool
+//
+//  Created by Superman on 2018/7/2.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecoardTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecoardTool
+//
+//  Created by Superman on 2018/7/2.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
